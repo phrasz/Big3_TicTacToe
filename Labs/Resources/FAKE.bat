bf00a0ba1d6bcaa4b7a20e83fb56039b22abf1c2
phrasz <phrasz@github.com>
2013-07-31 01:32:42 -0400

    Wrote the Lab for Java; port these for C/C++ and Python!

---
+++ Labs/Resources/FAKE.bat
@@ -0,0 +1,15 @@
+@echo off
+cls
+echo Microsoft Windows [Version 6.2.9200]
+echo (c) 2012 Microsoft Corporation. All rights reserved.
+echo.
+echo C:\TicTacToe^>javac TicTacToe.java
+echo TicTacToe.java:153: error: illegal character: \92
+echo message = "Instructions:\n\n" + \
+echo                                 ^^
+echo TicTacToe.java:154: error: not a statement
+echo "Your goal is to be the first player to get 3 X's or O's in a\n" +
+echo                                                                ^^
+echo 2 errors
+echo. 
+echo C:\TicTacToe^>
